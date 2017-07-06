4f479582b1eddbf15a50d5d8075d6efda849718b
Jake Lee <shibas11@gmail.com>
2017-07-07 01:57:36 +0900

    add test- gutterGame

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class GameTest {
 
     private Game game;
@@ -15,9 +18,19 @@ public class GameTest {
         game.roll(0);
     }
 
+    @Test
+    public void gutterGame() {
+        for (int i = 0; i < 20; i++)
+            game.roll(0);
+        assertThat(game.score(), is(0));
+    }
+
     private class Game {
         public void roll(int pins) {
+        }
 
+        public Integer score() {
+            return 0;
         }
     }
 }
